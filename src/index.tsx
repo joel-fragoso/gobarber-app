35d064537509d20bb67205a979733379ed459a8e
Joel Fragoso <joelfragoso85@icloud.com>
2020-07-03 13:33:55 -0300

    chore: first commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from "react";
+import { SafeAreaView, View, Text } from "react-native";
+
+const App: FunctionComponent = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello GoStack!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
