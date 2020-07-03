4dde69a96c7275d3b7ddc80a4a7e67efb5c67765
Joel Fragoso <joelfragoso85@icloud.com>
2020-07-03 16:10:25 -0300

    feat: pixel density

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,6 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent } from 'react';
 
-import { Container } from "./styles";
+import { Container } from './styles';
 
 const SignUp: FunctionComponent = () => {
   return <Container />;
