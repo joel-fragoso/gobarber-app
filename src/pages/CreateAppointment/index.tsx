28e0e92dd9c53a4d80db2bc35476c7bf61fc59de
Joel Fragoso <joelfragoso85@icloud.com>
2020-08-10 17:08:36 -0300

    feat: select provider

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,17 +1,92 @@
-import React, { FunctionComponent } from 'react';
-import { Button } from 'react-native';
+import React, {
+  FunctionComponent,
+  useState,
+  useEffect,
+  useCallback,
+} from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Title } from './styles';
+import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+  ProvidersListContainer,
+  ProvidersList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderName,
+} from './styles';
+
+export interface IProvider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
+interface IRouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: FunctionComponent = () => {
-  const { signOut } = useAuth();
+  const { user } = useAuth();
+  const route = useRoute();
+  const routeParams = route.params as IRouteParams;
+  const { goBack } = useNavigation();
+
+  const [providers, setProviders] = useState<IProvider[]>([]);
+  const [selectedProvider, setSelectedProvider] = useState(
+    routeParams.providerId,
+  );
+
+  useEffect(() => {
+    api.get('/providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
+
+  const handleSelectProvider = useCallback((providerId: string) => {
+    setSelectedProvider(providerId);
+  }, []);
+
+  const navigateToGoBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
 
   return (
     <Container>
-      <Title>CreateAppointment</Title>
-      <Button title="Sair" onPress={signOut} />
+      <Header>
+        <BackButton onPress={navigateToGoBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+      <ProvidersListContainer>
+        <ProvidersList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={providers}
+          keyExtractor={provider => provider.id}
+          renderItem={({ item: provider }) => (
+            <ProviderContainer
+              onPress={() => handleSelectProvider(provider.id)}
+              selected={provider.id === selectedProvider}
+            >
+              <ProviderAvatar source={{ uri: provider.avatar_url }} />
+              <ProviderName selected={provider.id === selectedProvider}>
+                {provider.name}
+              </ProviderName>
+            </ProviderContainer>
+          )}
+        />
+      </ProvidersListContainer>
     </Container>
   );
 };
--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -1,9 +1,20 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RectButton } from 'react-native-gesture-handler';
+
+import { IProvider } from '.';
+
+interface ISelectedProviderContainerProps {
+  selected: boolean;
+}
+
+interface IProviderNameProps {
+  selected: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  padding: 0 30px;
 `;
 
 export const Title = styled.Text`
@@ -12,4 +23,62 @@ export const Title = styled.Text`
   font-family: 'RobotoSlab-Regular';
   font-size: 18px;
   color: #fff;
+`;
+
+export const Header = styled.View`
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+  background: #28262e;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const HeaderTitle = styled.Text`
+  margin-left: 16px;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 20px;
+  color: #f4ede8;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  margin-left: auto;
+`;
+
+export const ProvidersListContainer = styled.View`
+  height: 112px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<IProvider>)`
+  padding: 32px 24px;
+`;
+
+export const ProviderContainer = styled(RectButton)<
+  ISelectedProviderContainerProps
+>`
+  background: ${props => (props.selected ? '#ff9000' : '#3e3b47')};
+  flex-direction: row;
+  align-items: center;
+  padding: 8px 12px;
+  margin-right: 16px;
+  border-radius: 10px;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+`;
+
+export const ProviderName = styled.Text<IProviderNameProps>`
+  margin-left: 8px;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 16px;
+  color: ${props => (props.selected ? '#232129' : '#f4ede8')};
 `;
